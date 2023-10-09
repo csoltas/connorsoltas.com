17e7b1d3131d1c79e7ad9c712aa48a2859251e68
Connor Soltas <connorsoltas@Connors-MacBook-Pro.local>
2023-10-09 14:21:14 -0400

    More changed to history API code

--- public/js/main.js
+++ public/js/main.js
@@ -247,7 +247,7 @@ function loadPage(page) {
 
       // Add new entry to history
       const url = window.location.pathname;
-      history.pushState({pageURL: url}, null, url);
+      history.pushState({pageURL: url}, "", url);
     });
 }
 
